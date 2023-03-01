f75546bd279102c24597b13a9344ac8b9c382c8f
balajipublic008 <balajipublic008@gmail.com>
2023-03-01 19:14:28 +0530

    gke-bala

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project     = "cloud-sql-project-379012"
   region      = "us-central1"
-  credentials = file("D:\DEV-OPS\terraform\provision-gke-cluster-balu\balaji_credentials.json")
+  credentials = file("D:\\DEV-OPS\\terraform\\provision-gke-cluster-balu\\balaji_credentials.json")
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 project_id = "cloud-sql-project-379012"
 region     = "us-central1"
+api_names = ["gkehub.googleapis.com"]

5ea042332f14e6f74a55c68374a4d58dae69cc53
balajipublic008 <balajipublic008@gmail.com>
2023-03-11 14:36:05 +0530

    provider credentials update_2

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@
 provider "google" {
   project     = "cloud-sql-project-379012"
   region      = "us-central1"
-  credentials = file("D:\\DEV-OPS\\terraform\\provision-gke-cluster-balu\\balaji_credentials.json") 
+  credentials = file("balaji_credentials.json") 
 }
